49d1fac2ac5424853c2fec89f6fd49637c3dcbaf
Arumutha Devi <arumuthadevi@gmail.com>
2026-06-28 18:03:22 +0530

    update files

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+   const buttons = document.querySelectorAll(".btn-primary, .btn-secondary, .order-btn");
+
+buttons.forEach(function(btn) 
+    btn.addEventListener("click", function () {
+        alert("🚀 Feature coming soon!")
+    });
+);
+</script>
